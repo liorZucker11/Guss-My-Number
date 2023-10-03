85e5cb1952901d57014c5b866ceb3198d3516290
liorzucker <93440030+liorzucker@users.noreply.github.com>
2023-10-03 14:23:30 +0300

    update documation

--- script.js
+++ script.js
@@ -1,117 +1,152 @@
 'use strict';
-let min=0 , max=20;
-let numberToGuss=  Math.floor(Math.random() * (max - min + 1)) + min ,score = 20 ,highScore=0 , tempScore=20, flagToClose=true;
 
+// Define initial variables for the game.
+let min = 0, max = 20;
+let numberToGuess = Math.floor(Math.random() * (max - min + 1)) + min;
+let score = 20, highScore = 0, tempScore = 20;
+let flagToClose = true;
+
+// Add event listeners to various elements in the HTML document.
 document.querySelector(".check").addEventListener("click", checkPress);
 document.querySelector(".again").addEventListener("click", againPress);
 document.querySelector(".setting").addEventListener("click", openSetting);
-document.querySelector(".close").addEventListener("click",closeModel)
-document.getElementById("blurModal").addEventListener("click",closeModel)
-document.querySelector(".save").addEventListener("click",savePress);
-document.addEventListener("keydown",function(e){
-    if(e.key==="Escape"){
+document.querySelector(".close").addEventListener("click", closeModel);
+document.getElementById("blurModal").addEventListener("click", closeModel);
+document.querySelector(".save").addEventListener("click", savePress);
+
+// Add keyboard event listeners for the 'Escape' key and 'Enter' key.
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape") {
         closeModel();
     }
-    if(e.key==="Enter"){
+    if (e.key === "Enter") {
         savePress();
     }
 });
-function savePress(){
-    let scoreDemo=0 , minDemo=0 , maxDemo=0;
-    let scoreElement=document.getElementById("newScore").value , minElement=document.getElementById("newMin").value , maxElement=document.getElementById("newMax").value;
 
-    if(scoreElement!=""){
-        scoreDemo=Number(scoreElement);
-    }else{scoreDemo=tempScore;}
-    if(scoreDemo<1){
-        displayMassage( "messageSetting" , "🛑 Score Must have to be up to 1 ..." )
-        flagToClose=false;
-    }else{
-        score=scoreDemo;
-        tempScore=scoreDemo;
+// Function to handle the 'Save' button click in the settings modal.
+function savePress() {
+    let scoreDemo = 0, minDemo = 0, maxDemo = 0;
+    let scoreElement = document.getElementById("newScore").value;
+    let minElement = document.getElementById("newMin").value;
+    let maxElement = document.getElementById("newMax").value;
+
+    // Check if a valid score is entered, else use the previous score.
+    if (scoreElement !== "") {
+        scoreDemo = Number(scoreElement);
+    } else {
+        scoreDemo = tempScore;
+    }
+
+    // Check if the entered score is greater than or equal to 1.
+    if (scoreDemo < 1) {
+        displayMessage("messageSetting", "🛑 Score must be 1 or more...");
+        flagToClose = false;
+    } else {
+        score = scoreDemo;
+        tempScore = scoreDemo;
         againPress();
     }
-    if(minElement!=""){
-        minDemo=Number(minElement);
-    }else{minDemo=min;}
-    if(maxElement!=""){
-        maxDemo=Number(maxElement);
-    }else{maxDemo=max;}
-    if(minDemo>=maxDemo){
-        displayMassage( "messageSetting" , "🛑 Min have to be less then Max ..." )
-        flagToClose=false;
+
+    // Check if valid minimum and maximum values are entered.
+    if (minElement !== "") {
+        minDemo = Number(minElement);
+    } else {
+        minDemo = min;
     }
-    else{
-        min=minDemo;
-        max=maxDemo;
-        document.querySelector('.between').innerHTML = `(Between ${min} and ${max})`;    
+    if (maxElement !== "") {
+        maxDemo = Number(maxElement);
+    } else {
+        maxDemo = max;
+    }
+
+    // Check if min is less than max.
+    if (minDemo >= maxDemo) {
+        displayMessage("messageSetting", "🛑 Min must be less than Max...");
+        flagToClose = false;
+    } else {
+        min = minDemo;
+        max = maxDemo;
+        document.querySelector('.between').innerHTML = `(Between ${min} and ${max})`;
         againPress();
     }
-    if(flagToClose){
+
+    // Close the settings modal if there are no errors.
+    if (flagToClose) {
         closeModel();
     }
-    flagToClose=true;
+    flagToClose = true;
 }
-function openSetting(){
+
+// Function to open the settings modal.
+function openSetting() {
     settingsModal.style.display = "block";
     blurModal.style.display = "block";
 }
 
-function againPress(){
+// Function to reset the game.
+function againPress() {
     document.querySelector('.score').textContent = tempScore;
-    score=tempScore;
-    numberToGuss= Math.floor(Math.random() * (max - min + 1)) + min;
+    score = tempScore;
+    //new number to guess
+    numberToGuess = Math.floor(Math.random() * (max - min + 1)) + min;
+    //reset the bg color.
     document.querySelector("body").style.backgroundColor = '#222';
-    document.querySelector(".number").textContent="?";
-    displayMassage("message","Start guessing...");
-    document.querySelector(".guess").value="";
-
+    document.querySelector(".number").textContent = "?";
+    //reset the msg.
+    displayMessage("message", "Start guessing...");
+    document.querySelector(".guess").value = "";
 }
 
-function checkPress(){
+// Function to handle the 'Check' button click.
+function checkPress() {
     const numberUser = Number(document.querySelector(".guess").value);
-    if(score>0){
-        if (!numberUser){
-            displayMassage("message","⚠ No Number!")
-        }
-        else if (numberUser<min || numberUser >max){
-            displayMassage("message","⚠ Number is not in the range!")
-        }
-        else if (numberToGuss === numberUser){
-            displayMassage("message","🏆 Correct you win!")
+    if (score > 0) {
+        //check if the user enter a valid number.
+        if (!numberUser) {
+            displayMessage("message", "⚠ No Number!");
+            //check if the number that the user pick is in the range , In order not to waste a turn.
+        } else if (numberUser < min || numberUser > max) {
+            displayMessage("message", "⚠ Number is not in the range!");
+            // if the user guess the right number.
+        } else if (numberToGuess === numberUser) {
+            displayMessage("message", "🏆 Correct! You win!");
             document.querySelector("body").style.backgroundColor = '#79AC78';
-            document.querySelector(".number").textContent=numberToGuss;
-            if(highScore<score){
-                 highScore=score;
+            document.querySelector(".number").textContent = numberToGuess;
+            if (highScore < score) {
+                highScore = score;
             }
-            document.querySelector(".highscore").textContent=highScore;
-            score=-1;
-
-        }
-        else if (numberUser!==numberToGuss){
-            displayMassage("message",numberUser>numberToGuss?"📈 To High!":"📉 To Low!");
+            document.querySelector(".highscore").textContent = highScore;
+            score = -1;
+            // if the user do not guess the right number. he get a relevent msg high or low.
+        } else if (numberUser !== numberToGuess) {
+            displayMessage("message", numberUser > numberToGuess ? "📈 Too High!" : "📉 Too Low!");
             pointReduction();
         }
     }
 }
 
-function displayMassage( obj , msg ){
+// Function to display messages.
+function displayMessage(obj, msg) {
     document.querySelector(`.${obj}`).textContent = msg;
 }
 
-function pointReduction(){
+// Function to reduce points. check if score is zero then present a losing msg.
+function pointReduction() {
     score--;
-    if(score===0){
-        displayMassage("message","👎 You Snooze Lose, try again!");
-        document.querySelector(".number").textContent=numberToGuss;
+    if (score === 0) {
+        displayMessage("message", "👎 You Lose! Try again.");
+        document.querySelector(".number").textContent = numberToGuess;
     }
     document.querySelector('.score').textContent = score;
 }
-function closeModel(){
+
+// Function to close the settings modal.
+function closeModel() {
     blurModal.style.display = "none";
     settingsModal.style.display = "none";
-    document.getElementById("newScore").value="";
-    document.getElementById("newMin").value="";
-    document.getElementById("newMax").value="";
-    document.getElementById("messageSetting").textContent="";
+    document.getElementById("newScore").value = "";
+    document.getElementById("newMin").value = "";
+    document.getElementById("newMax").value = "";
+    document.getElementById("messageSetting").textContent = "";
 }
